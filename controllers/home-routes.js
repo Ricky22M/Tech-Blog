db07fc730947b48e6be11ac31907cdf73f5fc2c8
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-12-25 19:50:50 -0700

    organized code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,30 +3,32 @@ const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'created_at'
-        ],
-        include: [{
-            model: Comment,
+    Post.findAll(
+        {
             attributes: [
                 'id',
-                'comment_text',
-                'post_id',
-                'user_id',
+                'title',
+                'content',
                 'created_at'
             ],
-            include: {
-                model: User,
+            include: [{
+                model: Comment,
                 attributes: [
-                    'username'
-                ]
-            }
-        }]
-    }).then(dbPostData => {
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: [
+                        'username'
+                    ]
+                }
+            }]
+        }
+    ).then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('homepage', { posts, loggedIn: req.session.loggedIn });
     }).catch(err => {
@@ -48,35 +50,37 @@ router.get('/signup', (req, res) => {
 });
 
 router.get('/post/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'content',
-            'title',
-            'created_at'
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
-                include: {
+    Post.findOne(
+        {
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: [
+                            'username'
+                        ]
+                    }
+                },
+                {
                     model: User,
                     attributes: [
                         'username'
                     ]
-                }
-            },
-            {
-                model: User,
-                attributes: [
-                    'username'
-                ]
-            }       
-        ]
-    }).then(dbPostData => {
+                }       
+            ]
+        }
+    ).then(dbPostData => {
         if (!dbPostData) {
             res.status(404).json({ message: 'No posts have been found with this id. Please try again!'});
             return;
