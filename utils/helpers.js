62e057fafe66c06f2e274f8e560378a6fae5abd8
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-12-28 15:15:11 -0700

    added all views files

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,9 @@
 module.exports = {
     formatted_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
-    }
+    },
 
-    formatted_word: (words, amount) => {
+    formatted_words: (words, amount) => {
         if (amount !== 1) {
             return `${words}s`;
         }
